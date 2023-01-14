b267cc589abbe5350dd11b8c1a1fd4df6cc7b24b
visill <victorkremensky@mail.ru>
2023-01-14 16:07:29 +0700

    tag + comment

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+from taggit.managers import TaggableManager
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
@@ -26,6 +28,8 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    tags = TaggableManager()
+
     objects = models.Manager()
     published = PublishedManager()
     class Meta:
@@ -41,4 +45,19 @@ class Post(models.Model):
                              self.publish.day,
                              self.slug
                              ])
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=254)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
     
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+
+    'taggit',
 ]
 
 MIDDLEWARE = [
